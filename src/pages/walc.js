08561a7aec5da82fa77a7f821fad4f93d3df59a7
jiahui <huan1681@purdue.edu>
2021-09-18 14:12:43 -0400

    make data file for possible study rooms and start on page to show

---
+++ src/pages/walc.js
@@ -0,0 +1,30 @@
+import React from "react";
+import STUDYAREA from "../data/locations";
+import {Button, Card, CardActions, CardContent, Typography, Chip} from "@material-ui/core";
+
+function WalcStudyArea(props) {
+    cont {location} = props;
+    if (location === undefined) {
+        return (
+            <div style={{flex: 1}} />
+        )
+    }
+    return (
+        <div style={{flex: 1, marginLeft: 10, overflowX: 'auto'}}>
+            <Typography variant={'h5'}>
+                {location.name}
+            </Typography>
+            {location.library ?
+                <Typography variant={"subtitle2"}>
+                    {location.library}
+                </Typography> : <></>
+            }
+            {location.capacity ?
+                <Typography variant={"body2"}>
+                    {location.capacity}
+                </Typography> : <></>
+            }
+
+
+    )
+}
