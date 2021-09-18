c6252cc6f3250404d01bd4c0ad14462dbb3306fd
jiahui <huan1681@purdue.edu>
2021-09-18 15:12:46 -0400

    possible page with fixes again

--- src/pages/walc.js
+++ src/pages/walc.js
@@ -3,13 +3,22 @@ import STUDYAREA from "../data/locations";
 import {Button, Card, CardActions, CardContent, Typography, Chip} from "@material-ui/core";
 
 function WalcStudyArea(props) {
-    cont {location} = props;
+    const {location} = props;
     if (location === undefined) {
         return (
             <div style={{flex: 1}} />
         )
     }
     return (
+    <Card style={{margin: 5, flex: 1}}>
+                <CardContent>
+                    <div style={{display: 'flex'}}>
+                        <div style={{maxHeight: 200, maxWidth: 150, overflow: 'hidden'}}>
+                            <div style={{display: 'flex', justifyContent: 'center'}}>
+                                <img src={process.env.PUBLIC_URL + person.picture} alt={'student profile'} height={200}/>
+                            </div>
+                        </div>
+
         <div style={{flex: 1, marginLeft: 10, overflowX: 'auto'}}>
             <Typography variant={'h5'}>
                 {location.name}
@@ -30,6 +39,10 @@ function WalcStudyArea(props) {
                 </Typography> : <></>
             }
         </div>
+        </div>
+        </CardContent>
+        </Card>
+
 
 
     )
